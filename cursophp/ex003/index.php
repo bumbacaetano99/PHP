58056ffccda5273bff73308eb5743980bf0fb8b1
bumbacaetano99 <bumbacaetano02@gmail.com>
2023-08-27 00:15:06 +0100

    php

---
+++ cursophp/ex003/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercicio </title>
+</head>
+<body>
+    <h1>Teste de tipos primitivos</h1>
+    <?php 
+   // $valor= 6e2;
+    //var_dump($valor)
+   echo  "PHP \u{1F418}";
+    
+    
+    
+    ?>
+</body>
+</html>
---
+++ cursophp/teste/teste.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+//$curso= "PHP";
+//$ano= date('Y');
+//Heredoc
+
+//echo <<< FRASE
+  //  Estou Estudando
+    // $curso em $ano
+    //FRASE;
+    
+    $resp=base_convert(254, 10, 16);
+     echo"o resultado é $resp";
+    
+    ?>
+</body>
+</html>
